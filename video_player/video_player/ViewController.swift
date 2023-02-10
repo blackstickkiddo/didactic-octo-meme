2b591a4a9cba3cb269d574dd3efbc7f0542a666f
blackstickkiddo <111551912+blackstickkiddo@users.noreply.github.com>
2023-02-10 16:40:12 -0500

    Add files via upload

---
+++ video_player/video_player/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Naïka Estriplet on 2022-09-18.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+ 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo(){
+        guard let path=Bundle.main.path(forResource: "test", ofType:"mp4") else {
+            debugPrint("test.mp4 not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
+
